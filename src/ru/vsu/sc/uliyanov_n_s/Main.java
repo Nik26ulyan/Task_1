330cc029f6df9e1095c647a713d8d7b4edcfed0f
Nik26ulyan <itishnik_nik@mail.ru>
2021-09-15 14:14:33 +0300

    v1.02

--- src/ru/vsu/sc/uliyanov_n_s/Main.java
+++ src/ru/vsu/sc/uliyanov_n_s/Main.java
@@ -5,15 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        findAverageNumb();
-    }
-
-    public static void findAverageNumb() {
         int[] nums = new int[3];
 
         System.out.println("Enter 3 numbers:");
         for (int i = 0; i < 3; i++){
-            nums[i] = inNum();
+            nums[i] = readNum();
         }
 
         int firstPairMax = findMaxValue(nums[0], nums[1]);
@@ -24,24 +20,28 @@ public class Main {
         int secondPairMin = findMinValue(nums[1], nums[2]);
         int minNum = findMinValue(firstPairMin, secondPairMin);
 
-        int averageNum = (nums[0] + nums[1] + nums[2]) - (minNum + maxNum);
+        int averageNum = findAverageNumb(nums[0], nums[1], nums[2], minNum, maxNum);
 
         System.out.print("Average number is " + averageNum);
     }
 
-    public static int findMaxValue(int a, int b) {
+    static int findAverageNumb(int a, int b, int c, int d, int e) {
+        return a + b + c - (d + e);
+    }
+
+    static int findMaxValue(int a, int b) {
         return (a + b)/2 + mod(a - b)/2 ;
     }
 
-    public static int findMinValue(int a, int b) {
+    static int findMinValue(int a, int b) {
         return (a + b)/2 - mod(a - b)/2 ;
     }
 
-    public static int mod(int a) {          //Не использую функцию Math.abs() т.к. там есть условный оператор.
+    static int mod(int a) {          //Не использую функцию Math.abs() т.к. там есть условный оператор.
         return (int) Math.sqrt(a * a);
     }
 
-    public static int inNum() {
+    static int readNum() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextInt();
     }
