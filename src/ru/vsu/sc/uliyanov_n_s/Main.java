21d72f74f4386b28f75dd35b839472b357653587
Nik26ulyan <itishnik_nik@mail.ru>
2021-09-16 23:40:13 +0300

    v1.2

--- src/ru/vsu/sc/uliyanov_n_s/Main.java
+++ src/ru/vsu/sc/uliyanov_n_s/Main.java
@@ -20,23 +20,23 @@ public class Main {
         printResult(averageNum);
     }
 
-    static void printResult(int a){
-        System.out.println("Average number is " + a +".");
+    static void printResult(int a) {
+        System.out.println("Average number is " + a + ".");
     }
 
-    static void printSomething(){
+    static void printSomething() {
         System.out.println("Enter 3 integer numbers");
     }
 
     static int findMaxValue(int a, int b, int c) {
-        int firstPairMax = ((a + b) + calculateModule(a - b))/ 2;
+        int firstPairMax = ((a + b) + calculateModule(a - b)) / 2;
         int secondPairMax = ((a + c) + calculateModule(a - c)) / 2;
         return ((firstPairMax + secondPairMax) + calculateModule(firstPairMax - secondPairMax)) / 2;
     }
 
     static int findMinValue(int a, int b, int c) {
         int firstPairMin = ((a + b) - calculateModule(a - b)) / 2;
-        int secondPairMin = ((a + c)- calculateModule(a - c)) / 2;
+        int secondPairMin = ((a + c) - calculateModule(a - c)) / 2;
         return ((firstPairMin + secondPairMin) - calculateModule(firstPairMin - secondPairMin)) / 2;
     }
 
