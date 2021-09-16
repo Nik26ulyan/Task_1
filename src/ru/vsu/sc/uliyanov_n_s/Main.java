69f20e1a762591fab434d41f4ad8b066a21f57d9
Nik26ulyan <itishnik_nik@mail.ru>
2021-09-16 22:11:03 +0300

    v1.2

--- src/ru/vsu/sc/uliyanov_n_s/Main.java
+++ src/ru/vsu/sc/uliyanov_n_s/Main.java
@@ -1,71 +1,56 @@
 package ru.vsu.sc.uliyanov_n_s;
 
-import com.sun.org.apache.xpath.internal.objects.XString;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        printSomething("Enter 3 integer numbers:");
+        printSomething();
 
-        int num1 = readNumForComparison("first");
-        int num2 = readNumForComparison("second");
-        int num3 = readNumForComparison("third");
+        int num1 = readNumForCompare("first");
+        int num2 = readNumForCompare("second");
+        int num3 = readNumForCompare("third");
 
-        int firstPairMax = findMaxValueInPair(num1, num2);
-        int secondPairMax = findMaxValueInPair(num2, num3);
-        int maxNum = findMaxValueInPair(firstPairMax, secondPairMax);
+        int maxNum = findMaxValue(num1, num2, num3);
+        int minNum = findMinValue(num1, num2, num3);
 
-        int firstPairMin = findMinValueInPair(num1, num2);
-        int secondPairMin = findMinValueInPair(num2, num3);
-        int minNum = findMinValueInPair(firstPairMin, secondPairMin);
-
-        int averageNum = findAverageNumb(num1, num2, num3, minNum, maxNum);
+        int averageNum = findAverageNumb(num1, num2, num3, maxNum, minNum);
 
         printResult(averageNum);
     }
 
+    static void printResult(int a){
+        System.out.println("Average number is " + a +".");
+    }
+
+    static void printSomething(){
+        System.out.println("Enter 3 integer numbers");
+    }
+
+    static int findMaxValue(int a, int b, int c) {
+        int firstPairMax = ((a + b) + calculateModule(a - b))/ 2;
+        int secondPairMax = ((a + c) + calculateModule(a - c)) / 2;
+        return ((firstPairMax + secondPairMax) + calculateModule(firstPairMax - secondPairMax)) / 2;
+    }
+
+    static int findMinValue(int a, int b, int c) {
+        int firstPairMin = ((a + b) - calculateModule(a - b)) / 2;
+        int secondPairMin = ((a + c)- calculateModule(a - c)) / 2;
+        return ((firstPairMin + secondPairMin) - calculateModule(firstPairMin - secondPairMin)) / 2;
+    }
+
     static int findAverageNumb(int a, int b, int c, int d, int e) {
-        return a + b + c - (d + e);
+        return (a + b + c) - (d + e);
     }
 
-    static int findMaxValueInPair(int a, int b) {
-        return (a + b) / 2 + CalcModuleMethod(a - b) / 2;
-    }
-
-    static int findMinValueInPair(int a, int b) {
-        return (a + b) / 2 - CalcModuleMethod(a - b) / 2;
-    }
-
-    static int CalcModuleMethod(int a) {          //Не использую функцию Math.abs() т.к. там есть условный оператор.
+    static int calculateModule(int a) {          //Не использую функцию Math.abs() т.к. там есть условный оператор.
         return (int) Math.sqrt(a * a);
     }
 
-    static void printSomething(String text){
-        System.out.println();
-    }
-
-    static void printResult(int a) {
-        System.out.print("Average number is " + a);
-    }
-
-    static int readNumForComparison(String text) {
+    static int readNumForCompare(String text) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the " + text + " number");
-        return scanner.nextInt();
-    }
-
-    static int readNum2() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter second number");
-        return scanner.nextInt();
-    }
-
-    static int readNum3() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter third number");
+        System.out.print("Enter the " + text + " number:");
         return scanner.nextInt();
     }
 }
